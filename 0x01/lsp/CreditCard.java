01c84f3cd757bf4f551329e0b6246ffc6bda6239
brunomanente <bruno.manente0@gmail.com>
2022-04-06 16:32:04 -0300

    Liskov Substitution Principle - LSP

--- 0x01/lsp/CreditCard.java
+++ 0x01/lsp/CreditCard.java
@@ -1,5 +1,6 @@
 
-public class CreditCard extends BankCard {
+public class CreditCard extends BankCard{
+	
 	public void validate() {
 		System.out.println("Verificando Limite!");
 		System.out.println("Limite disponível!");
