4f4f154f13123aa98925195929f1b8d5b164be01
brunomanente <bruno.manente0@gmail.com>
2022-04-07 14:40:27 -0300

    DIP

---
+++ 0x02/dip/MySQLProductRepository.java
@@ -0,0 +1,20 @@
+
+public class MySQLProductRepository implements IDbProduct {
+	 public String getProductById(String productID) {
+		return productID;
+		 
+	}
+	 public String getAll() {
+		return null;
+		 
+	}
+		public String delete(String productID) {
+			return productID;
+			
+		}
+	
+	public String update(Product product) {
+		return null;
+		
+	}
+}
