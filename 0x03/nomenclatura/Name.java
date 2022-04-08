c1f9d3495842cf2a3aea4e2b10e217342822534a
brunomanente <bruno.manente0@gmail.com>
2022-04-08 12:00:41 -0300

    Uso da Nomenclatura

--- 0x03/nomenclatura/Name.java
+++ 0x03/nomenclatura/Name.java
@@ -4,23 +4,31 @@ import java.sql.Date;
 import java.util.List;
 
 public class Name {
+	
+	    public Date d; // modified date
+	    public List<Testcase> find(Name.User user){}; //find all Testcases by User
 
-	    public Date modifiedDate;
-	    public List<Testcase> findAllTestcasesByUser(Name.User user){};
 	  
-	    public List<Account> AccountList;
+	    public Account[] AccountList; 
+
 	   
-	    public void deleteUser(Name.User pUser){}; 
-	  
+	    public void deleteUser(Name.User user){};
+
+	   
 	    public URL url;
-	 
-	    public class TableRepresent {};
-	    public void saveCar(){};
+
 	   
-	    public saveUser(){}; saveAccount(){}; saveContract(){};
-		 
-	    public int DAYS_IN_YEAR = 360;
-	    public int hoursInWeek = 168;
+	    public class TableRepresenter{};
+	    public void car(){}; 
+
 	  
+	    public saveUser(){}; createAccount(){}; generateContract(){};
+
+
+	     
+	    public final int DAYS_IN_YEAR = 360;
+	    public final int HOURS_IN_WEEK = 168;
 	}
+	  
+	
 
