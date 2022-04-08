9eab7b92af35631bba588dad5c05b9fc95eff72a
brunomanente <bruno.manente0@gmail.com>
2022-04-08 11:49:47 -0300

    Uso da Nomenclatura

---
+++ 0x03/nomenclatura/Name.java
@@ -0,0 +1,26 @@
+
+import java.net.URL;
+import java.sql.Date;
+import java.util.List;
+
+public class Name {
+
+	    public Date modifiedDate;
+	    public List<Testcase> findAllTestcasesByUser(Name.User user){};
+	  
+	    public List<Account> AccountList;
+	   
+	    public void deleteUser(Name.User pUser){}; 
+	  
+	    public URL r;
+	 
+	    public class TableRepresent {};
+	    public void saveCar(){};
+	   
+	    public saveUser(){}; saveAccount(){}; saveContract(){};
+		 
+	    public int daysInYear = 360;
+	    public int hoursInWeek = 168;
+	  
+	}
+
