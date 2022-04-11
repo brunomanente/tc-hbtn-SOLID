1c139caec247396ddbdb10325fad9f1e944e2658
bmanente <bmanente@ciandt.com>
2022-04-11 13:34:34 -0300

    Convenções de Nomenclatura

--- 0x04/convencao/User.java
+++ 0x04/convencao/User.java
@@ -8,8 +8,16 @@ private String descricao;
 
 final static int IDADEMINIMA = 18;
 
+// função que coleta todos os users
+
+public FetchUsers() { return [];}
+
+// função que coleta todos os users
+
 public AllUsers() {return [];}
 
+// função que coleta todos os users
 
+public GetUsers() {return [];}
 
 }
