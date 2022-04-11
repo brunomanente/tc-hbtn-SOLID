c4f101844920daec9dda1ac0ca5b4f57f1122835
bmanente <bmanente@ciandt.com>
2022-04-11 13:33:48 -0300

    Convenções de Nomenclatura

---
+++ 0x04/convencao/User.java
@@ -0,0 +1,15 @@
+// classe users, representando nossa entidade
+
+public class UsersModels {
+
+private int idUser;
+
+private String descricao;
+
+final static int IDADEMINIMA = 18;
+
+public AllUsers() {return [];}
+
+
+
+}
