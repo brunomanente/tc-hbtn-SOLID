fc4bc702db32e55dd7dadaae4e2c42de2392f45c
bmanente <bmanente@ciandt.com>
2022-04-11 13:21:22 -0300

    excluindo comentarios

---
+++ 0x04/cleanCode/Comments.java
@@ -0,0 +1,27 @@
+public class Comments {
+
+
+    // this method delivers owner of testcase
+    public Owner getTestcaseOwner(Testcase testcase){}; 
+
+
+    // returns hours in month
+    public Hours getHoursInQuarter(){}; 
+
+    // TODO Este métod deve ser removido
+    public void check(){
+        return;
+    }
+
+
+    /**
+     * <p> this method publishes results based on date
+     * </p>
+     * @param date date from when results should be published
+     * @return List of Result entities
+     * @since 16.0
+     */
+    public List<Result> publishResults(Date date) {
+       
+    }
+}
